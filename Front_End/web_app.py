8f3671e0bffd364c9b2613a86de8fc2a21194daa
ASHSWAN1999 <ashley.swanson@students.olin.edu>
2018-04-30 23:00:56 -0400

    finially pushing the updates of the webapp

--- Front_End/web_app.py
+++ Front_End/web_app.py
@@ -4,14 +4,32 @@ from flask import (Flask, render_template, redirect,
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-# @app.route('/save', methods=['POST'])
-# def save():
-#   response = make_response(redirect(url_for('index')))
-#   response.set_cookie('character', json.dumps(dict(request.form.items())))
-#   return response
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/record')
+def record():
+    return render_template('audio_recorder/record.html')
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie('character', json.dumps(dict(request.form.items())))
+    return response
+
+
+@app.route('/test')
+def test():
+    return render_template('audio_recorder/ar.html')
+
 
 app.run(debug=True, host="0.0.0.0", port=8000)
