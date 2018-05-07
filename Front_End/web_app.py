12401b83ea96e24af23831d74e4730bd2ef75cbe
ASHSWAN1999 <ashley.swanson@students.olin.edu>
2018-05-07 16:32:36 -0400

    Commented webapp

--- Front_End/web_app.py
+++ Front_End/web_app.py
@@ -7,23 +7,24 @@ from predict_class import main_given_filename
 
 app = Flask(__name__)
 
+lang='' #global variable for passing current accent guess between pages
 
-@app.route('/')
+@app.route('/') #home page
 def index():
     return render_template('index.html')
 
 
-@app.route('/about')
+@app.route('/about') #takes user to github website
 def about():
     return render_template('about.html')
 
 
-@app.route('/record')
+@app.route('/record') #page where user records passage
 def record():
     return render_template('audio_recorder/record.html')
 
 
-@app.route('/r', methods = ['GET', 'POST'])
+@app.route('/r', methods = ['GET', 'POST']) #uploads file name for processing
 def upload_file():
    if request.method == 'POST':
       try:
@@ -33,16 +34,14 @@ def upload_file():
       except:
           return redirect('/record')
 
-      #JJ do some stuff
-      #print(f.filename)
-      filepath = '../../Desktop/' + f.filename
+      print(f.filename)
+      filepath = '../../Downloads/' + f.filename
+      global lang
       lang = main_given_filename(filepath)
-
-      #lang = 'Danish'
       return render_template('result.html', answer=lang)
 
 
-@app.route('/result')
+@app.route('/result') #page with accent guess
 def result():
     return render_template('result.html')
 
@@ -51,7 +50,8 @@ def answer():
    if request.method == 'POST':
       first_lang = request.form['a']
       print(first_lang)
-      return redirect('/result')
+      global lang
+      return render_template('result.html', answer=lang)
 
 
 app.run(debug=True, host="0.0.0.0", port=8000)
